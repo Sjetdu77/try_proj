f834157392258457edf1984e9d07e247e5b8899f
Sjetdu77 <andy.frot@outlook.fr>
2021-12-08 11:27:27 +0100

    Adds

--- index.py
+++ index.py
@@ -1,10 +1,20 @@
-from flask import Flask, request, render_template as rt, jsonify, redirect, url_for
+from flask import Flask, request, render_template, jsonify, redirect, url_for, jsonify
+import json
 
 app = Flask(__name__)
+infos = json.load(open('data/infos.json'))
 
 @app.route('/')
 def index():
-	return "Hello!"
+	return render_template("index.html", infos=infos)
+
+@app.route('/stats/')
+def statsGen():
+	return render_template("allStats.html", infos=infos)
+
+@app.route('/stats/<character>')
+def statsChar(character):
+	return render_template("stat.html", name=character, infos=infos[character])
 
 if __name__ == "__main__":
 	app.run(debug=True, host='0.0.0.0', port=5100)
